f03cd03c731b022f9eed3225179247b8cf8e8b91
Gill Cleeren <gill@snowball.be>
2020-08-23 12:15:21 +0200

    Extra data

--- GloboTicket/GloboTicket.Services.EventCatalog/DbContexts/EventCatalogDbContext.cs
+++ GloboTicket/GloboTicket.Services.EventCatalog/DbContexts/EventCatalogDbContext.cs
@@ -20,6 +20,7 @@ namespace GloboTicket.Services.EventCatalog.DbContexts
             var concertGuid = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}");
             var musicalGuid = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}");
             var playGuid = Guid.Parse("{BF3F3002-7E53-441E-8B76-F6280BE284AA}");
+            var conferenceGuid = Guid.Parse("{FE98F549-E790-4E9F-AA16-18C2292A2EE9}");
 
             modelBuilder.Entity<Category>().HasData(new Category
             {
@@ -36,6 +37,11 @@ namespace GloboTicket.Services.EventCatalog.DbContexts
                 CategoryId = playGuid,
                 Name = "Plays"
             });
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = conferenceGuid,
+                Name = "Conferences"
+            });
 
             modelBuilder.Entity<Event>().HasData(new Event
             {
@@ -45,7 +51,7 @@ namespace GloboTicket.Services.EventCatalog.DbContexts
                 Artist = "John Egbert",
                 Date = DateTime.Now.AddMonths(6),
                 Description = "Join John for his farwell tour across 15 continents. John really needs no introduction since he has already mesmerized the world with his banjo.",
-                ImageUrl = "/img/banjo.jpg",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/banjo.jpg",
                 CategoryId = concertGuid
             });
 
@@ -57,14 +63,49 @@ namespace GloboTicket.Services.EventCatalog.DbContexts
                 Artist = "Michael Johnson",
                 Date = DateTime.Now.AddMonths(9),
                 Description = "Michael Johnson doesn't need an introduction. His 25 concert across the globe last year were seen by thousands. Can we add you to the list?",
-                ImageUrl = "/img/michael.jpg",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/michael.jpg",
                 CategoryId = concertGuid
             });
 
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                EventId = Guid.Parse("{B419A7CA-3321-4F38-BE8E-4D7B6A529319}"),
+                Name = "Clash of the DJs",
+                Price = 85,
+                Artist = "DJ 'The Mike'",
+                Date = DateTime.Now.AddMonths(4),
+                Description = "DJs from all over the world will compete in this epic battle for eternal fame.",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/dj.jpg",
+                CategoryId = concertGuid
+            });
 
             modelBuilder.Entity<Event>().HasData(new Event
             {
                 EventId = Guid.Parse("{62787623-4C52-43FE-B0C9-B7044FB5929B}"),
+                Name = "Spanish guitar hits with Manuel",
+                Price = 25,
+                Artist = "Manuel Santinonisi",
+                Date = DateTime.Now.AddMonths(4),
+                Description = "Get on the hype of Spanish Guitar concerts with Manuel.",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/guitar.jpg",
+                CategoryId = concertGuid
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                EventId = Guid.Parse("{1BABD057-E980-4CB3-9CD2-7FDD9E525668}"),
+                Name = "Techorama 2021",
+                Price = 400,
+                Artist = "Many",
+                Date = DateTime.Now.AddMonths(10),
+                Description = "The best tech conference in the world",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/conf.jpg",
+                CategoryId = conferenceGuid
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                EventId = Guid.Parse("{ADC42C09-08C1-4D2C-9F96-2D15BB1AF299}"),
                 Name = "To the Moon and Back",
                 Price = 135,
                 Artist = "Nick Sailor",
@@ -72,8 +113,7 @@ namespace GloboTicket.Services.EventCatalog.DbContexts
                 Description = "The critics are over the moon and so will you after you've watched this sing and dance extravaganza written by Nick Sailor, the man from 'My dad and sister'.",
                 ImageUrl = "/img/musical.jpg",
                 CategoryId = musicalGuid
-            });    
+            });
         }
     }
-
 }
