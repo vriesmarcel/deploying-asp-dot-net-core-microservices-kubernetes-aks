c9e26d3d535b897f91c89786bcf8ef34ecfee83e
Marcel de Vries <vriesmarcel@hotmail.com>
2020-10-31 22:48:09 +0000

    Update load.sh

--- load.sh
+++ load.sh
@@ -5,7 +5,7 @@ rate: $max calls / second"
 START=$(date +%s);
 
 get () {
-  curl -s -v "$1" 2>&1 | tr '\r\n' '\\n' | awk -v date="$(date +'%r')" '{print $0"\n-----", date}' >> /tmp/perf-test.log
+  curl -s -v "$1" 2>&1 | tr '\r\n' '\\n' | awk -v date="$(date +'%r')" '{print $0"\n-----", date}' 
 }
 
 while true
