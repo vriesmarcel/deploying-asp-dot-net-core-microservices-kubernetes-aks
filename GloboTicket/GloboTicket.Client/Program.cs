d2040ce3549039860be13268e0ad600789a48d8a
Marcel de Vries <vriesmarcel@hotmail.com>
2020-11-18 22:25:32 +0100

    Change web app to run on ssl with provided certificate from environment

--- GloboTicket/GloboTicket.Client/Program.cs
+++ GloboTicket/GloboTicket.Client/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
 
 namespace GloboTicket.Web
 {
@@ -7,14 +9,26 @@ namespace GloboTicket.Web
     {
         public static void Main(string[] args)
         {
+            WriteBase64PfxFileFromEnvironmentToDisk();
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        private static void WriteBase64PfxFileFromEnvironmentToDisk()
+        {
+            var base64pfxfile = Environment.GetEnvironmentVariable("base64pfxfile");
+            if (!string.IsNullOrWhiteSpace(base64pfxfile))
+            {
+                Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Path");
+                var filename = Path.Combine(Directory.GetCurrentDirectory(), "certfile.pfx");
+                File.WriteAllBytes(filename, Convert.FromBase64String(base64pfxfile));
+                Environment.SetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Path", filename);
+            }
+        }
+            public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
+        }
     }
-}
